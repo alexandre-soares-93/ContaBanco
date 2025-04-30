8f90247c77fa4bd92daa9dabb6feb02a40ef4fc6
alexandre-soares-93 <asoaresdeoliveira977@gmail.com>
2025-04-30 18:38:58 -0300

    Criação da jornada de abertura de conta v1, aplicando conceitos de java básico

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ContaTerminal {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Informe seu nome completo: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite o número de sua agência com digito: ");
+        String agencia = scanner.next();
+
+        System.out.print("Informe a sua conta: ");
+        int conta = scanner.nextInt();
+
+        System.out.print("Faça seu primeiro depósito: R$");
+        float saldo = scanner.nextFloat();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " +
+                agencia + ", conta " + conta + " e seu saldo R$" + saldo + " já está disponivel para saque.");
+
+    }
 }
